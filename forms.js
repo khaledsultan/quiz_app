97301797aa757e20e46f71e71ffd1a82f1ed49d7
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-06 16:09:27 +0200

    quiz

--- forms.js
+++ forms.js
@@ -1,13 +1,14 @@
 
 
 
+// -------Add-A-QUESTION--------
 
 
 const test = document.querySelector('[data-js="test"]');
 const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    console.log("submit");
+    // console.log("submit");
     const formElements = e.target.elements;
     // console.log("formElements", formElements);
 
@@ -22,7 +23,7 @@ form.addEventListener("submit", (e) => {
 
     const newSection = document.createElement("section");
     // newSection.classList.add("body","product");
-    test.append(newSection);
+    biggerForm.append(newSection);
     // test.append(newSection);
     newSection.innerHTML= `
             <a href="" class="bookmark" data-js="bookmark">🔖</a>
@@ -33,6 +34,82 @@ form.addEventListener("submit", (e) => {
             <ul>
                 <a href="">#${userTag}</a>
             </ul>`
-     console.log(newSection);       
-     
+    //  console.log(newSection);       
+    form.reset( )
 });
+
+// -------COUNTER--------
+
+
+// text.addEventListener("input",(evt) =>{
+//     let a =characterCounter(evt.target.value);
+//     const counterBox = document.querySelector("p");
+//     if (counterBox.textContent == `${a} characters left`) {
+//         counterBox.textContent ==``;
+//     }else{
+//     counterBox.textContent = `${a} character left`;
+// }
+// })
+
+
+
+
+//  -------------------------------
+let  text = document.getElementById('userQuestion');
+let  text_2 =document.getElementById('userAnswer');
+
+
+
+
+
+
+text.addEventListener("input",(evt) => {
+    characterCounter(evt.target)
+})
+text_2.addEventListener("input",(evt) => {
+    characterCounter(evt.target)
+})
+
+
+function characterCounter(input) {
+    let b = 150 - input.value.length ;
+
+    let counterBox = document.querySelector("p");
+
+    if (input == text) {
+        counterBox = document.querySelector("p");
+    } else {
+        counterBox = document.getElementById("text_2");
+    }
+    counterBox.textContent == `${b} characters left`
+    if (counterBox.textContent == `${b} characters left`) {
+        counterBox.textContent ==``;
+    }else{
+        if (b == 1) {
+            counterBox.textContent = `${b} character left`;
+             } else{
+             counterBox.textContent = `${b} characters left`;
+             }
+    }
+    
+    return counterBox
+
+}
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+// function characterCounter(x) {
+//     let characterLeft = 150 - x.length ;
+//     return   characterLeft 
+    
+// }
